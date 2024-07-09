7f8e0a55662a4a18270d0fefb2aa19aeb8fcd64f
kazuddd <daud.oscc@gmail.com>
2024-07-09 15:56:17 +0700

    [feat] Implementasi middleware admin & subadmin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // Custom middleware
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'subadmin' => \App\Http\Middleware\SubAdminMiddleware::class,
     ];
 }
